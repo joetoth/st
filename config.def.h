5778dbc593ddc303b7ebc13124ce1734fd125497
Joe Toth <joetoth@gmail.com>
2017-03-15 21:36:38 -0400

    scp

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Mono:pixelsize=10:antialias=true:autohint=true";
+static char font[] = "Source Code Pro:pixelsize=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
